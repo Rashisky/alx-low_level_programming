cc78a0c6a4247a97bf74f44b972b803f9b1e88c4
Rashisky <rashisky007@gmail.com>
2022-03-12 00:28:00 +0100

    Compiling

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,6 +6,7 @@
 /*
  * betty style doc for function main goes there
  * main - Calculate if n is positive or negative
+ * main function - calculate either positive or negative of the number
  * return - Always 0 (Success), print either the value of the number
  */
 
