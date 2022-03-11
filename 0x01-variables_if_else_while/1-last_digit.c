735cc73c4cbd40681350f2a5a6e5ec3bd56262bd
Rashisky <rashisky007@gmail.com>
2022-03-12 00:42:18 +0100

    Compiling

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,27 +12,32 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */ 
+	/* your code goes there */
 	l = n%10;
-	if (n > 0)
+if (n > 0)
 	{
 		if (l > 5)
-			printf("%s %d %s %d %s", "Last digit of", n, "is", l, "and is greater than 5");
+			printf("%s %d %s %d %s\n", "Last digit of", n,\
+				       	"is", l, "and is greater than 5");
 		else
 			if (l>0 && l<6)
-             			printf("%s %d %s %d %s", "Last digit of", n, "is", l, "and is less than 6 and not 0");
+             			printf("%s %d %s %d %s\n", "Last digit of", \
+						n, "is", l, "and is less than 6 and not 0");
 			else 
-				printf("%s %d %s %d %s", "Last digit of", -n, "is", l, "and is 0");
+				printf("%s %d %s %d %s\n", "Last digit of", -n, "is", l, "and is 0");
 				
 	}
 	else
 		if (l > 5)
-                        printf("%s %d %s %d %s", "Last digit of", n, "is", l, "and is greater than 5");
+                        printf("%s %d %s %d %s\n", "Last digit of", \
+					n, "is", l, "and is greater than 5");
 	       	else
-			if (l==0)
-				printf("%s %d %s %d %s", "Last digit of", n, "is", l, "and is 0");
+			if (l == 0)
+				printf("%s %d %s %d %s\n", "Last digit of", \
+						n, "is", l, "and is 0");
 			else
-				printf("%s %d %s %d %s", "Last digit of", n, "is", l, "and is less than 6 and not 0");
+				printf("%s %d %s %d %s\n", "Last digit of", \
+						n, "is", l, "and is less than 6 and not 0");
 
 	return (0);
 }
