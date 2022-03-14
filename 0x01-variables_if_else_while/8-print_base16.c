863462a4da589e74960f34a87249d29faae7372e
Rashisky <rashisky007@gmail.com>
2022-03-14 09:31:25 +0100

    C variables

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,8 +3,17 @@
 /**
  * main - prints number of base 16
  * in lower case
- * result - always 0 (Success)
+ * Result: always 0 (Success)
  */
 int main(void)
 {
+	int k;
+	char n;
 
+	for (k = '0'; k <= '9'; k++)
+		putchar(k);
+	for (n = 'a'; n <= 'f'; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
