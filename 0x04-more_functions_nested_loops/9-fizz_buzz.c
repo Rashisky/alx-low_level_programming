f419c805a4212833273ce7100729320f301b3eef
Rashisky <rashisky007@gmail.com>
2022-03-20 02:46:20 +0100

    variable loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,11 +5,10 @@
  * main - prints 1 to 100, with multiples of 3 replaced
  * with Fizz, multiples of 5 replaced with Buzz and
  * multiples of 5,3 replaced with FizzBuzz
- * @n: integer number
  * Return: 0 means success
  */
 
-int main(int n)
+int main(void)
 {
 	int k;
 
@@ -28,16 +27,7 @@ int main(int n)
 				printf("%d ", k);
 		}
 		else
-		{
-                        if (k % 3 == 0 && k % 5 == 0)
-                                printf("FizzBuzz");
-                        else if (k % 5 == 0)
-                                printf("Buzz");
-                        else if (k % 3 == 0)
-                                printf("Fizz ");
-                        else
-                                printf("%d", k);
-		}
+			printf("Buzz");
 	}
 	putchar('\n');
 
