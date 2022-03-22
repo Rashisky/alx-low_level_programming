51ded86f599681d1353a38f22cbc3517767ba064
Rashisky <rashisky007@gmail.com>
2022-03-22 14:55:25 +0100

    Pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_rev - print reverse characters.
+ * @s: checks an argument
+ * Return: Always 0.
+ */
+
+int main()
+{
+	string a = "lobby";
+
+	reverse(a);
+	return 0;
+}
