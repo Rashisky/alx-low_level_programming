a7e20c135d047b6285dcd132655bf744fef30643
Rashisky <rashisky007@gmail.com>
2022-04-05 17:04:00 +0100

    memory allocation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,9 +20,9 @@ char *str_concat(char *s1, char *s2)
 	n = 0;
 
 	if (s1 == NULL)
-		return ("");
+		s1 = "";
 	if (s2 == NULL)
-		return ("");
+		s2 = "";
 	while (s1[i] != '\0')
 		i++;
 	while (s2[k] != '\0')
