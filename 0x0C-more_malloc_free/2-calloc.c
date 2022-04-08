f5360a65bedef60ea4b70182f4f15b1d1c61bb8d
Rashisky <rashisky007@gmail.com>
2022-04-08 08:53:08 +0100

    more malloc functions

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	sum = nmemb * size;
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	mem = malloc((nmemb + 1) * sum);
+	mem = malloc((nmemb * sum);
 	if (mem == NULL)
 		return (NULL);
 	while (t < sum)
