dde9efb7651d28e986d2e3bc33eab5cebaffa761
Rashisky <rashisky007@gmail.com>
2022-04-12 10:31:23 +0100

    Structure (struct)

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,20 @@
 #ifndef __STRUCT_DOG__
 #define __STRUCT_DOG__
 
+
+/**
+ * struct  dog - struct function that initializes a variable type struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: This takes in 3 arguments declaration
+ */
+
 struct dog
 {
-	char *name; 
-	float age; 
+	char *name;
+	float age;
 	char *owner;
 };
 
