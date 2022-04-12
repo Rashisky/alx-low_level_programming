cb5ab2e82cc7eca24ef05a27c361db434b5bd2ae
Rashisky <rashisky007@gmail.com>
2022-04-12 10:20:15 +0100

    Structure (struct)

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+THIS IS PROJECTS ON STRUC, TYPEDEF etc
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,11 @@
+#ifndef __STRUCT_DOG__
+#define __STRUCT_DOG__
+
+struct dog
+{
+	char *name; 
+	float age; 
+	char *owner;
+};
+
+#endif
