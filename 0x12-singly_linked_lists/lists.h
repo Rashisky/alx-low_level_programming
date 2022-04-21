f2bc5388061186f071ccd8a7d2ebbe1c238c60dd
Rashisky <rashisky007@gmail.com>
2022-04-22 00:15:12 +0100

    single linked items

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,20 @@
 #ifndef _RASHISKY_
 #define _RASHISKY_
-#include <string.h>
 
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+/**
+ * struct list - A user defined data type
+ * @str: string of the first argument
+ * @len: The lenght of the string argument
+ * @next: A pointer to struct list that points to next node
+ *
+ * Description: This is a user defined data type whose name
+ * was later changed to list_t using typedef
+ */
 
 typedef struct list
 {
