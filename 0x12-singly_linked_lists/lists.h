e23169b42f3b20989b73673b2d8e0e3426a37b94
Rashisky <rashisky007@gmail.com>
2022-04-22 00:06:20 +0100

    single linked items

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,10 +2,14 @@
 #define _RASHISKY_
 #include <string.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 typedef struct list
 {
     char *str;
-    int len;
+    unsigned int len;
     struct list *next;
 } list_t;
 size_t print_list(const list_t *h);
