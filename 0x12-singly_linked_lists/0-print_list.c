ddc8463910674d1e225722c66cb72ebd13ddc489
Rashisky <rashisky007@gmail.com>
2022-04-22 00:00:41 +0100

    single linked items

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,25 +8,26 @@
  * print_list - function that prints all the elements of a list_t list
  * @h: Takes in a pointer to struct list_t parameter
  * if str is NULL, print [0] (nil)
- * 
+ *
  * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
-    
-        while(h != NULL)
-        {    
-            if (h->str == NULL)
-            {
-                printf("[%d] %s\n", 0, "(nil)");
-            }
-            else
-            {
-                printf("[%d] %s\n", h->len, h->str);
-            }
-            h = h->next;
-            count += 1;
-        }
-        return (count);
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count += 1;
+	}
+
+	return (count);
 }
