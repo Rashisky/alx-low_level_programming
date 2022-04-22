f646bea939d6cad09fba9ca928cc365346ec9580
Rashisky <rashisky007@gmail.com>
2022-04-22 22:02:47 +0100

    Single linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,22 +11,22 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    size_t i = 0;
-    list_t *ret = (list_t *)malloc(sizeof(list_t));
+	size_t i = 0;
+	list_t *ret = (list_t *)malloc(sizeof(list_t));
 
-    if (ret == NULL)
-    {
-        return (NULL);
-    }
-    while(str[i])
-    {
-        i++;
-    }
-    ret->str =  (char *)str;
-    ret->len = i;
-    ret->next = *head;
-    /* Where address *head = 00000000 */
-    /* and while the value is NULL */
+	if (ret == NULL)
+	{
+		return (NULL);
+	}
+	while (str[i])
+	{
+		i++;
+	}
+	ret->str =  (char *)str;
+	ret->len = i;
+	ret->next = *head;
+	/* Where address *head = 00000000 */
+	/* and while the value is NULL */
 
-    return(ret);
+	return (ret);
 }
