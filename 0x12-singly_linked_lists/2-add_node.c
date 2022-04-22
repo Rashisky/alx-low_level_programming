953976dca32ec3a2445d2648c6952061068a562b
Rashisky Paper <rashisky007@gmail.com>
2022-04-22 21:50:51 +0100

    Single link list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+
+/**
+ * add_node - function that adds a new node at the
+ * beginning of a list_t list
+ * @head: double pointer of data type list_t that
+ * if str is NULL, print [0] (nil)
+ *
+ * Return: the number of nodes
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+    size_t i = 0;
+    list_t *ret = (list_t *)malloc(sizeof(list_t));
+
+    if (ret == NULL)
+    {
+        return (NULL);
+    }
+    while(str[i])
+    {
+        i++;
+    }
+    ret->str =  (char *)str;
+    ret->len = i;
+    ret->next = *head;
+    /* Where address *head = 00000000 */
+    /* and while the value is NULL */
+
+    return(ret);
+}
