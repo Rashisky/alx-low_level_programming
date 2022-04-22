3858d1f1030e81d2319d7cc99a17c627f9c25a40
Rashisky <rashisky007@gmail.com>
2022-04-22 22:07:48 +0100

    Single linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,6 +5,7 @@
  * add_node - function that adds a new node at the
  * beginning of a list_t list
  * @head: double pointer of data type list_t that
+ * @str: The string contained in the node
  * if str is NULL, print [0] (nil)
  *
  * Return: the number of nodes
@@ -22,11 +23,11 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		i++;
 	}
-	ret->str =  (char *)str;
+	ret->str =  strdup(str);
 	ret->len = i;
 	ret->next = *head;
 	/* Where address *head = 00000000 */
 	/* and while the value is NULL */
 
-	return (ret);
+	return (ret->next);
 }
