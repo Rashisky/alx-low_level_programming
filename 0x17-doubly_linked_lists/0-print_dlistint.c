fb98fc3e941248d5f48e2fb67e0805e90aa5e90d
root <root@Rashisky-Paper.localdomain>
2022-06-10 23:04:31 +0100

    Doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,6 +12,8 @@ size_t print_dlistint(const dlistint_t *h)
 	dlistint_t *temp = (dlistint_t *)h;
 	size_t count = 0;
 
+	if (h == NULL)
+		return (count);
 	while (temp != NULL)
 	{
 		printf("%u\n", temp->n);
