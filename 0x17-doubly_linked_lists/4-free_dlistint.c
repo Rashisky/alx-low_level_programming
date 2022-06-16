98fbb7ac073bbdb4c8dd1e2c72d3a1fdf758ddc9
Rashisky <root@Rashisky-Paper.localdomain>
2022-06-16 21:46:10 +0100

    Doubly linked lists

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -10,18 +10,17 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *temp, *del;
+	dlistint_t *temp;
 
 	while (head->prev != NULL)
 		head = head->prev;
 	temp = head;
 	while (temp->next != NULL)
 		temp = temp->next;
-	while (temp != head)
+	while (temp->prev != NULL)
 	{
 		temp = temp->prev;
-		del = temp->next;
-		free(del);
+		free(temp->next);
 	}
 	free(head);
 }
