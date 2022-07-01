54c9a9c883d5686d64da55ca151615d4878ac545
Rashisky <root@Rashisky-Paper.localdomain>
2022-07-01 21:23:48 +0100

    Hash tables

---
+++ 0x01-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hast_table_create - Creates a hash table
+ * @size: The size of the array
+ *
+ * Return: pointer to the new hash table.
+ * else Null
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
+
+	ht = malloc(sizeof(has_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if(ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
+}
---
+++ 0x01-hash_tables/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+Introduction to hash table
