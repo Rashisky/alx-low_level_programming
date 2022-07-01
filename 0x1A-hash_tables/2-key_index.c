912f8a3f3679353ba3537966c0299c295727c85c
Rashisky <root@Rashisky-Paper.localdomain>
2022-07-01 21:44:00 +0100

    Hash tables

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,7 +9,7 @@
  * Return: The index of the key.
  * Description: Uses the djb2 algorithm
  */
-unsigned long int key_index(const unsigned char *key, unsigned lon int size)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
 }
