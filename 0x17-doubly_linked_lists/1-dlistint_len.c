1eaecfd2d43b0ea04248eff7314f4a1c834d98ed
Rashisky <rashisky007@gmail.com>
2023-01-23 13:27:54 +0100

    List lenght

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,26 +1,26 @@
 #include "lists.h"
 
 /**
- * _dlistint_len - function that prints all the elements of a dlistint_t list
+ * dlistint_len - function that prints all the elements of a dlistint_t list
  * @h: pointer to the first node
+ *
+ * Return: size of dlistint_len element
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    dlistint_t *temp;
-    int count = 0;
+	dlistint_t *temp;
+	int count = 0;
 
-    if (h)
-    {
-        temp = (dlistint_t *)h;
+	if (h)
+	{
+		temp = (dlistint_t *)h;
 
-        while (temp != NULL)
-        {
-            temp = temp->next;
-            count += 1;
-        }
-
-        return count;
-    }
-
-    return (count);
+		while (temp != NULL)
+		{
+			temp = temp->next;
+			count += 1;
+		}
+		return (count);
+	}
+	return (count);
 }
