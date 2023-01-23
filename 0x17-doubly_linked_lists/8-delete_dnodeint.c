7badd130b93f2359a3e4b82f20d23d140296f4cd
Rashisky <rashisky007@gmail.com>
2023-01-23 13:23:27 +0100

    Delete at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - function that deletes a node at a given index of dlistint
+ * delete_dnodeint_at_index - function that deletes a node at a given
+ * index of dlistint
  * @head: pointer to the first node
  * @index: index of the node to be deleted
  *
@@ -9,41 +10,27 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *last, *temp = *head;
-    dlistint_t *node;
-    unsigned int count = 0;
+	dlistint_t *last, *temp = *head;
+	unsigned int count = 0;
 
-    if (temp)
-    {
-        node = malloc(sizeof(dlistint_t));
-        if (!(node))
-            return (7);
-        while (temp->next)
-        {
-            if (count == index)
-           {
-                last = (temp)->prev;
-                temp = (temp)->next;
-                (temp)->prev = last;
+	while (temp)
+	{
+		if (count == index)
+		{
+			last = temp->prev;
+			if (!(last))
+				*head = temp->next;
+			else
+				last->next = temp->next;
+			if (temp->next)
+				temp->next->prev = last;
+			free(temp);
+			temp = NULL;
+			return (1);
+		}
+		count++;
+		temp = temp->next;
+	}
 
-                printf("value of index and count are:%d and %d\n", index, count);
-                while (count > 0)
-                {
-                    printf("In while loop\n");
-                    *head = (*head)->prev;
-                    printf("The value of (*head->n) is %d\n", (*head)->n);
-                      count -= 1;
-                    printf("count = %d\n", count);
-                }
-
-                return (1);
-            }
-
-            count += 1;
-            temp = (temp)->next;
-            *head = temp;
-        }
-        return (-1);
-    }
-    return (-1);
+	return (-1);
 }
