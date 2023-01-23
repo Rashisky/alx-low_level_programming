0ac55e36532e5a13c4c0b021228f2a7f909af16f
Rashisky <rashisky007@gmail.com>
2023-01-23 13:23:48 +0100

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,61 +1,57 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - function that inserts a new node at a given position
+ * insert_dnodeint_at_index - function that inserts a new node
+ * at a given position
  * @h: pointer to the first node
- * @idx: index of the node to be inserted
+ * @idx: index of the node to be inserted (starting from 0)
  * @n: data of the given node
  *
  * Return: Address of the new node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    unsigned int count = 0;
-    dlistint_t *temp = *h;
-    dlistint_t *new_node;
+	unsigned int count = 0;
+	dlistint_t *temp = *h;
+	dlistint_t *new_node, *temp2 = temp;
 
-    new_node = malloc(sizeof(dlistint_t));
-
-    if (new_node == NULL)
-        return NULL;
-
-    new_node->n = n;
-    if (temp)
-    {
-        while (temp->next)
-        {
-            if (count == (idx - 1))
-            {
-                new_node->prev = temp;
-                new_node->next = temp->next;
-                temp->next = new_node;
-                return (*h);
-            }
-            count += 1;
-            temp = temp->next;
-        }
-
-        if (!(temp->next))
-        {
-            if (idx == 0)
-            {
-                new_node->next = (*h)->next;
-                new_node->prev = (*h)->prev;
-                *h = new_node;
-            }
-            else if ((count + 1) == idx)
-            {
-                new_node->next = temp->next;
-                new_node->prev = temp;
-                temp->next = new_node;
-            }
-            else
-                return (NULL);
-            return (*h);
-        }
-    }
-    new_node->prev = NULL;
-    new_node->next = NULL;
-    *h = new_node;
-    return (*h);
+	if (temp)
+	{
+		new_node = malloc(sizeof(dlistint_t));
+		if (new_node == NULL)
+			return (NULL);
+		new_node->n = n;
+		new_node->prev = NULL;
+		while (temp)
+		{
+			if (count == idx)
+			{
+				new_node->next = temp2->next;
+				if (temp->prev == NULL)
+					*h = new_node;
+				else
+				{
+					new_node->prev = temp2;
+					if (temp->prev)
+					{
+						new_node->prev = temp->prev;
+						temp->prev = new_node;
+					}
+					temp2->next = new_node;
+				}
+				return (*h);
+			}
+			temp2 = temp;
+			count += 1;
+			temp = temp->next;
+		}
+		if (count == idx)
+		{
+			new_node->next = temp;
+			new_node->prev = temp2;
+			temp2->next = new_node;
+			return (*h);
+		}
+	}
+	return (*h);
 }
