80ffc7e862dfd3be1668e4bf263b0e8de04d7a4e
Rashisky <rashisky007@gmail.com>
2023-02-24 22:44:30 +0100

    Makefiles

---
+++ 0x1C-makefiles/test_files/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int school(void);
+int main(void)
+{
+    puts("This is the main file\n");
+    school();
+    puts("End of main file\n");
+    return 0;
+}
+/* School */
+/*Rashisky*/
---
+++ 0x1C-makefiles/test_files/school.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int school(void)
+{
+    puts("This is the school file\n");
+    puts("End of school file\n");
+    return 0;
+}
