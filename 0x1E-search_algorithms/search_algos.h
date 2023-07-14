7fb6ccddc76c196e5ef6249f07a68a3e92955a7c
Rashisky <Rashisky007@gmail.com>
2023-07-14 23:50:43 +0100

    Header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,5 +7,9 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int bin_search(int *array, size_t min, size_t max, int value);
+int jump_search(int *array, size_t size, int value);
+int value_range(int *array, int min, int max, int size, int value);
+int interpolation_search(int *array, size_t size, int value);
+size_t position(int *array, int low, int high, size_t size, int value);
 
 #endif
